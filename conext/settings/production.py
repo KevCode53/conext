9208c3545c533aaaf5124d1edf0f2115739cdb36
Kevin Alberto Palma Ralda <kapalma05@gmail.com>
2022-09-23 21:58:56 -0600

    🛸 first commit

---
+++ conext/settings/production.py
@@ -0,0 +1,19 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-%$dn1dbov@x2ankg0=f0e!-s7h)f067@dgf*i51r4%ywsh47=m'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
